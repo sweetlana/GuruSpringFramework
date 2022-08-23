9aa5142e03de475a1297973dffcb10481daf5ff8
@sweetlana <lana.grivnak@gmail.com>
2022-08-23 13:53:02 +0300

    Create springboot package in guru.springframework

--- di/src/main/java/springboot/DependencyInjectionApplication.java
+++ di/src/main/java/springboot/DependencyInjectionApplication.java
@@ -1,13 +1,15 @@
-package guru.springframework.di;
+package springboot;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import guru.springframework.di.controllers.GreetingController;
 
 @SpringBootApplication
+@ComponentScan("guru.springframework")
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
