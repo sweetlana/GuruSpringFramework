9f25765740fa76d33e2244ddc83ea3b8c79b68d3
@sweetlana <lana.grivnak@gmail.com>
2022-08-24 16:05:58 +0300

    add Spring Beans in XML

--- di/src/main/java/guru/springframework/di/config/HelloConfig.java
+++ di/src/main/java/guru/springframework/di/config/HelloConfig.java
@@ -13,34 +13,34 @@ import guru.springframework.di.services.HelloWorldServiceSpanishImpl;
 @Configuration
 public class HelloConfig {
 	
-	@Bean
-	public HelloWorldFactory helloWorldFactory() {
-		return new HelloWorldFactory();
-	}
+//	@Bean
+//	public HelloWorldFactory helloWorldFactory() {
+//		return new HelloWorldFactory();
+//	}
 	
-	@Bean
-	@Profile("english")
-	@Primary
-	public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory) {
-		return factory.createHelloWorldService("en");
-	}
+//	@Bean
+//	@Profile("english")
+//	@Primary
+//	public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory) {
+//		return factory.createHelloWorldService("en");
+//	}
 	
-	@Bean
-	@Profile("spanish")
-	@Primary
-	public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory) {
-		return factory.createHelloWorldService("es");
-	}
+//	@Bean
+//	@Profile("spanish")
+//	@Primary
+//	public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory) {
+//		return factory.createHelloWorldService("es");
+//	}
 	
-	@Bean(name = "french")
-	public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory) {
-		return factory.createHelloWorldService("fr");
-	}
+//	@Bean(name = "french")
+//	public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory) {
+//		return factory.createHelloWorldService("fr");
+//	}
 	
-	@Bean
-	public HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory) {
-		return factory.createHelloWorldService("de");
-	}
+//	@Bean
+//	public HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory) {
+//		return factory.createHelloWorldService("de");
+//	}
 	
 	@Bean
 	public HelloWorldService helloWorldServicePolish(HelloWorldFactory factory) {
--- di/src/main/java/guru/springframework/di/controllers/GreetingController.java
+++ di/src/main/java/guru/springframework/di/controllers/GreetingController.java
@@ -7,26 +7,21 @@ import org.springframework.stereotype.Controller;
 import guru.springframework.di.services.HelloWorldService;
 import guru.springframework.di.services.HelloWorldServiceGermanImpl;
 
-@Controller
+//@Controller
 public class GreetingController {
 	
 	private HelloWorldService helloWorldService;
 	private HelloWorldService helloWorldServiceGerman;
 	private HelloWorldService helloWorldServiceFrench;
 
-	@Autowired
 	public void setHelloWorldService(HelloWorldService helloWorldService) {
 		this.helloWorldService = helloWorldService;
 	}
 
-	@Autowired
-	@Qualifier("helloWorldServiceGerman")
 	public void setHelloWorldServiceGerman(HelloWorldService helloWorldServiceGerman) {
 		this.helloWorldServiceGerman = helloWorldServiceGerman;
 	}
 	
-	@Autowired
-	@Qualifier("french")
 	public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
 		this.helloWorldServiceFrench = helloWorldServiceFrench;
 	}
