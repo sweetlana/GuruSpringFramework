6194d47a366ceac51f6a87df7d4f44846d98165e
@sweetlana <lana.grivnak@gmail.com>
2022-08-24 12:28:25 +0300

    add Spring XML Configuration

--- di/src/main/java/springboot/DependencyInjectionApplication.java
+++ di/src/main/java/springboot/DependencyInjectionApplication.java
@@ -4,12 +4,14 @@ package springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 import guru.springframework.di.controllers.GreetingController;
 
 @SpringBootApplication
-@ComponentScan("guru.springframework")
+//@ComponentScan("guru.springframework")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
